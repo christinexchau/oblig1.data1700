b6b842bd122afdf32be1303ae4bf4ed16f6b02d8
christinexchau <143170525+christinexchau@users.noreply.github.com>
2024-02-16 13:55:11 +0100

    valideringer

--- .idea/java.js
+++ .idea/java.js
@@ -1,1 +1,17 @@
-let telefonnr.
+if (!gyldigEpost(epost.value)) {
+    epostError.textContent = 'Vennligst skriv inn en gyldig e-postadresse.';
+    gyldigVerdi = false;
+} else {
+    epostError.textContent = '';
+}
+
+if (!gyldigTelefonnummer(telefonnr.value)) {
+    telefonnrError.textContent = 'Vennligst skriv inn et gyldig telefonnummer.';
+    gyldigVerdi = false;
+} else {
+    telefonnrError.textContent = '';
+}
+
+if (!gyldigVerdi) {
+    event.preventDefault();
+}
