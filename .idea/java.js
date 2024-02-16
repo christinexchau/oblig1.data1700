8fb5ed95d9beebce41c65d9add958cb07a31d38a
christinexchau <143170525+christinexchau@users.noreply.github.com>
2024-02-16 14:54:30 +0100

    nye verdier

--- .idea/java.js
+++ .idea/java.js
@@ -1,17 +1,37 @@
-if (!gyldigEpost(epost.value)) {
-    epostError.textContent = 'Vennligst skriv inn en gyldig e-postadresse.';
-    gyldigVerdi = false;
-} else {
-    epostError.textContent = '';
+let billetter = []; //lagre en array for billetter
+function kjøpBilletter() {
+
+    let form = document.getElementById('billettStruktur');
+
+    let fornavn = document.getElementById("fornavn");
+    let etternavn = document.getElementById("etternavn");
+    let epost = document.getElementById('epost');
+    let telefonnr = document.getElementById('telefonnr');
+    let antall = document.getElementById("antall");
+
+    let telefonnrRegex = document.getElementById("telefonnrRegex");
+    let epostRegex = document.getElementById("epostRegex");
+
+
+    if (!gyldigEpost(epost.value)) {
+        epostError.textContent = 'Vennligst skriv inn en gyldig e-postadresse.';
+        gyldigVerdi = false;
+    } else {
+        epostError.textContent = '';
+    }
+
+    if (!gyldigTelefonnummer(telefonnr.value)) {
+        telefonnrError.textContent = 'Vennligst skriv inn et gyldig telefonnummer.';
+        gyldigVerdi = false;
+    } else {
+        telefonnrError.textContent = '';
+    }
+
+    if (!gyldigVerdi) {
+        event.preventDefault();
+    }
 }
 
-if (!gyldigTelefonnummer(telefonnr.value)) {
-    telefonnrError.textContent = 'Vennligst skriv inn et gyldig telefonnummer.';
-    gyldigVerdi = false;
-} else {
-    telefonnrError.textContent = '';
-}
+function slettAlleBilletter () {
 
-if (!gyldigVerdi) {
-    event.preventDefault();
 }
